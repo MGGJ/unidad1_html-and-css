ddfe2d39da44395048b1b81f73a6572f6faaaa85
MGGJ <151755159+MGGJ@users.noreply.github.com>
2024-02-22 12:35:16 -0600

    Add files via upload

---
+++ Documentos/js javascript.js
@@ -0,0 +1,79 @@
+var nombre="guadalupe";
+var  edad=20;
+var estatura;
+
+let apellidos="gonzalez";
+let estatura=1.72;
+
+const ESCUELA='tecnm roque'
+
+
+
+ console.log ("nombre:" + nombre);
+ console.log ("edad:"+ edad);
+ console.log('String2'+ String);
+ console.log('apellido;'+apellidos);
+ console.log('estatura;'+estatura);
+ console.log(ESCUELA);
+
+ let String='cadenas de texto';
+ let booleans= true;
+ let booleans2=flase;
+ let number= 10;
+ let number2=20.5;
+ let bigint= new digInt(10);
+ let symnols= 1,20,30,40;
+ let arrays2= 1,'otros datos', false;
+ let objet={};
+
+ let object={
+    propiedad1:' valor de la propiedad',
+ };
+
+ let student={
+    name:'mauricio',
+    lastname:'gonzalez',
+     age: 20,
+    topics: 'programacion web','base de datos',
+
+    assesments:{
+        webdevelopemn: 10,
+        Math: 9,
+
+
+    },  
+
+ };
+
+ 
+
+ console.table(student{' name'});
+ console.log(student.name);
+ console.log(student.propiedad1);
+ 
+ student.log(student,name);
+
+ let obj1= {
+    propObj1: 'values 1';
+
+
+
+ };
+
+ let obj2={
+    propObj2:'values 2';
+    
+ }
+
+ let obj3={
+    prop3:'value'
+ }
+
+
+
+
+
+
+
+
+ 
